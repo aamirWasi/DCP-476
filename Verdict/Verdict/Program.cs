29e2934ec69465491befd5999f9b3949c1b1060d
aamir <wasi3aamir@gmail.com>
2019-01-23 00:58:46 +0600

    Accepted

---
+++ Verdict/Verdict/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verdict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            for(int t = 0; t < T; t++)
+            {
+                string line = Console.ReadLine();
+                if (line == "ac")
+                {
+                    Console.WriteLine("Accepted");
+                } else if(line == "wa")
+                {
+                    Console.WriteLine("Wrong Answer");
+                }
+                else if (line == "rte")
+                {
+                    Console.WriteLine("Run Time Error");
+                }
+                else if(line == "tle")
+                {
+                    Console.WriteLine("Time Limit Exceeded");
+                }
+
+            }
+        }
+    }
+}
